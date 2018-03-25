c6ec993c31f3f860f154c09c37ba60004e0e24f4
Alexander Danilin <dan-sasha@mail.ru>
2018-03-25 20:05:02 +0700

    Answer number check with good numbers

--- src/test/java/me/svistoplyas/AnswerTest.java
+++ src/test/java/me/svistoplyas/AnswerTest.java
@@ -24,7 +24,7 @@ class AnswerTest {
     @ParameterizedTest
     @MethodSource("badNumbers")
     @DisplayName("Number check")
-    public void numberTest(int num){
+    public void badNumberTest(int num){
         Assertions.assertThrows(IndexOutOfBoundsException.class, () -> new Answer(num));
     }
 
@@ -32,6 +32,23 @@ class AnswerTest {
         return Stream.of(
                            Arguments.of(-1),
                           Arguments.of(4)
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("goodNumbers")
+    @DisplayName("Good numbers check")
+    public void goodNumberTest(int num){
+        new Answer(num);
+    }
+
+    private static Stream<Arguments> goodNumbers() {
+        return Stream.of(
+                Arguments.of(0),
+                Arguments.of(1),
+                Arguments.of(2),
+                Arguments.of(3)
         );
     }
 }
