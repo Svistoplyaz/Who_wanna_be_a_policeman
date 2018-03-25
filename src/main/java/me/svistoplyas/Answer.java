77269ff6062465d6ef563e6c8b27f8fabc5b6f96
Alexander Danilin <dan-sasha@mail.ru>
2018-03-25 19:53:13 +0700

    Answer number check implemented

--- src/main/java/me/svistoplyas/Answer.java
+++ src/main/java/me/svistoplyas/Answer.java
@@ -7,6 +7,11 @@ public class Answer {
     int num;
 
     Answer(int n){
+        if(n < 0)
+            throw new IndexOutOfBoundsException("Negative number");
+
+        if(n > 3)
+            throw new IndexOutOfBoundsException("Number is too big");
         num = n;
     }
 }
