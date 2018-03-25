b7051c0b8e8ab61400b537b366dc3aae1a5440b1
Alexander Danilin <dan-sasha@mail.ru>
2018-03-25 19:51:09 +0700

    Creating answer with negative or too big number (Failed)

--- src/test/java/me/svistoplyas/AnswerTest.java
+++ src/test/java/me/svistoplyas/AnswerTest.java
@@ -1,5 +1,6 @@
 package me.svistoplyas;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,15 @@ class AnswerTest {
         new Answer(0);
     }
 
+    @Test
+    @DisplayName("Negative number")
+    public void numberTest1(){
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> new Answer(-1));
+    }
 
+    @Test
+    @DisplayName("Too big number")
+    public void numberTest2(){
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> new Answer(4));
+    }
 }
