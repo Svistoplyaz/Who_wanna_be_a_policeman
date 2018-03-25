1e546fa5fc353632b5601deca464285b3fcb200e
Alexander Danilin <dan-sasha@mail.ru>
2018-03-25 20:00:51 +0700

    Answer number check ParameterizedTest refactor

--- src/test/java/me/svistoplyas/AnswerTest.java
+++ src/test/java/me/svistoplyas/AnswerTest.java
@@ -3,6 +3,12 @@ package me.svistoplyas;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,15 +20,18 @@ class AnswerTest {
         new Answer(0);
     }
 
-    @Test
-    @DisplayName("Negative number")
-    public void numberTest1(){
-        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> new Answer(-1));
+
+    @ParameterizedTest
+    @MethodSource("badNumbers")
+    @DisplayName("Number check")
+    public void numberTest(int num){
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> new Answer(num));
     }
 
-    @Test
-    @DisplayName("Too big number")
-    public void numberTest2(){
-        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> new Answer(4));
+    private static Stream<Arguments> badNumbers() {
+        return Stream.of(
+                           Arguments.of(-1),
+                          Arguments.of(4)
+        );
     }
 }
