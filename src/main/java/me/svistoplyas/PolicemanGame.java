ed5cb4a8209af3672a8faa2834eb79e9dbe0bece
Alexander Danilin <dan-sasha@mail.ru>
2018-03-26 18:58:15 +0700

    PolicemanGame question array, implemented reading question set from file

--- src/main/java/me/svistoplyas/PolicemanGame.java
+++ src/main/java/me/svistoplyas/PolicemanGame.java
@@ -1,5 +1,28 @@
 package me.svistoplyas;
 
+import java.io.*;
+
 public class PolicemanGame {
     static int questionLimit = 10;
+    Question[] questions;
+
+    void readSet(File file) throws Exception {
+        BufferedReader in = new BufferedReader(new FileReader(file));
+
+        int len = Integer.parseInt(in.readLine());
+        questions = new Question[len];
+
+        String[] str;
+        for(int i = 0; i < len; i++){
+            str = in.readLine().split(" ");
+            questions[i] = new Question(i, str[0], Integer.parseInt(str[1]));
+
+            for(int j = 0; j < Question.answerLimit; j++)
+                questions[i].addAnswer(new Answer(j, in.readLine()));
+        }
+    }
+
+    public Question[] getQuestions() {
+        return questions;
+    }
 }
--- src/test/java/me/svistoplyas/PolicemanGameTest.java
+++ src/test/java/me/svistoplyas/PolicemanGameTest.java
@@ -2,11 +2,45 @@ package me.svistoplyas;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 class PolicemanGameTest {
-
     @Test
     public void mainClassInitialisation(){
         new PolicemanGame();
+    }
+
+    @Test
+    void readSet() throws Exception{
+        File file = new File("set2.in");
+        BufferedWriter out = new BufferedWriter(new FileWriter(file));
+        out.write("2\n" +
+                "Вопрос1 2\n" +
+                "йОтвет1\n" +
+                "цОтвет2\n" +
+                "уОтвет3\n" +
+                "кОтвет4\n" +
+                "Вопрос2 3\n" +
+                "еОтвет1\n" +
+                "нОтвет2\n" +
+                "гОтвет3\n" +
+                "шОтвет4\n");
+        out.close();
+
+        PolicemanGame game = new PolicemanGame();
+        game.readSet(file);
+
+        Question[] questions = game.getQuestions();
+
+        assertEquals("Вопрос1", questions[0].getValue());
+        assertTrue(questions[0].choose(2));
+
+        assertEquals("Вопрос2", questions[1].getValue());
+        assertTrue(questions[1].choose(3));
     }
 }
