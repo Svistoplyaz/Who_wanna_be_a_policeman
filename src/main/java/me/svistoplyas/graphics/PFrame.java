95a1979cc4c748ce4bb8ce05d645375c440a0f9c
Alexander Danilin <dan-sasha@mail.ru>
2018-03-27 12:36:22 +0700

    [Error]Revert "PFrame askNextQuestion infinite loop fixed"
    This reverts commit e26d9e923d3a80b670b2df9eafea87b85ad90b36.

--- src/main/java/me/svistoplyas/graphics/PFrame.java
+++ src/main/java/me/svistoplyas/graphics/PFrame.java
@@ -8,23 +8,23 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 //J means Java, P means Police:)
-public class PFrame extends JFrame {
+public class PFrame extends JFrame{
     private PolicemanGame game;
     private Question curQuestion;
     private JLabel qLabel;
     private ArrayList<PButton> buttons = new ArrayList<>();
 
-    public PFrame(PolicemanGame game) {
+    public PFrame(PolicemanGame game){
         this.game = game;
         setLayout(null);
-        this.setLocation(300, 300);
+        this.setLocation(300,300);
         curQuestion = game.getNextQuestion();
 
         qLabel = new JLabel("<html>" + curQuestion.getValue());
         qLabel.setBounds(20, 60, 330, 200);
         add(qLabel);
 
-        for (Answer answer : curQuestion.getAnswers()) {
+        for(Answer answer : curQuestion.getAnswers()){
             addButton(new PButton(answer));
         }
 
@@ -35,9 +35,9 @@ public class PFrame extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    private void addButton(PButton button) {
+    private void addButton(PButton button){
         int cur = buttons.size();
-        button.setBounds(20 + (cur % 2) * 220, 200 + (cur / 2) * 75, 200, 68);
+        button.setBounds(20 + (cur % 2) * 220, 200 + (cur/2) * 75, 200, 68);
         this.add(button);
         buttons.add(button);
     }
@@ -54,18 +54,16 @@ public class PFrame extends JFrame {
         return curQuestion;
     }
 
-    public void askNextQuestion() {
-        if (curQuestion != game.getQuestions()[game.getQuestions().length - 1]) {
-            curQuestion = game.getNextQuestion();
-            qLabel.setText("<html>" + curQuestion.getValue());
+    public void askNextQuestion(){
+        curQuestion = game.getNextQuestion();
+        qLabel.setText("<html>" + curQuestion.getValue());
 
-            int i = 0;
-            for (Answer answer : curQuestion.getAnswers()) {
-                buttons.get(i).setAnswer(answer);
-                i++;
-            }
-
-            repaint();
+        int i = 0;
+        for(Answer answer : curQuestion.getAnswers()){
+            buttons.get(i).setAnswer(answer);
+            i++;
         }
+
+        repaint();
     }
 }
