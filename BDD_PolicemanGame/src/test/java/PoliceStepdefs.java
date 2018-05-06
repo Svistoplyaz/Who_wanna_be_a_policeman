54c4872a759aa40e213fb8bcbcbf5f0579a7a3cd
Alexander Danilin <dan-sasha@mail.ru>
2018-05-06 17:04:53 +0700

    Checking number of loaded questions

--- BDD_PolicemanGame/src/test/java/PoliceStepdefs.java
+++ BDD_PolicemanGame/src/test/java/PoliceStepdefs.java
@@ -1,6 +1,8 @@
 import cucumber.api.PendingException;
 import cucumber.api.java.gl.E;
 import cucumber.api.java8.En;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 public class PoliceStepdefs implements En {
     private PolicemanGame policemanGame;
@@ -9,12 +11,16 @@ public class PoliceStepdefs implements En {
         Given("^I have my policeman game$", () -> {
             this.policemanGame = new PolicemanGame();
         });
-        And("^I load my set of question from this \"([^\"]*)\" file$", (String arg0) -> {
+        When("^I load my set of question from this \"([^\"]*)\" file$", (String arg0) -> {
             try {
                 policemanGame.load(arg0);
             }catch (Exception e){
                 e.printStackTrace();
             }
+        });
+        Then("^number of questions must be more then (\\d+)$", (Integer arg0) -> {
+            if(policemanGame.questions.length <= arg0)
+                Assertions.fail("Less than " + (arg0+1) + "questions")
         });
     }
 }
--- BDD_PolicemanGame/src/test/resources/police.feature
+++ BDD_PolicemanGame/src/test/resources/police.feature
@@ -2,4 +2,5 @@ Feature: PolicemanGame
 
   Scenario: game
     Given I have my policeman game
-    And I load my set of question from this "set1.in" file
+    When I load my set of question from this "set1.in" file
+    Then number of questions must be more then 0
